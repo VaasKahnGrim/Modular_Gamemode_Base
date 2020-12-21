e1cc388d3e404d4ce7e43274a31238d6e61e638a
Vaas_Kahn_Grim <29955667+VaasKahnGrim@users.noreply.github.com>
2020-12-21 01:47:09 -0600

    Create loader.lua

---
+++ gamemode/gm_flatgrass/loader.lua
@@ -0,0 +1,9 @@
+print("VaasGM | Starting Shared: gm_flatgrass")
+
+if SERVER then
+    print("VaasGM | Starting Serverside: gm_flatgrass")
+else
+    print("VaasGM | Starting Clientside: gm_flatgrass")
+end
+
+
