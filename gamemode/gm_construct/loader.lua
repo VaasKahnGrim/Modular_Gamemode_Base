275f8bb90a6174ca327e0935063a22f2f133f4d8
Vaas_Kahn_Grim <29955667+VaasKahnGrim@users.noreply.github.com>
2020-12-21 01:46:36 -0600

    Create loader.lua

---
+++ gamemode/gm_construct/loader.lua
@@ -0,0 +1,9 @@
+print("VaasGM | Starting Shared: gm_construct")
+
+if SERVER then
+    print("VaasGM | Starting Serverside: gm_construct")
+else
+    print("VaasGM | Starting Clientside: gm_construct")
+end
+
+
